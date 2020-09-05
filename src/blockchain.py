ae9b409f71a54895647aecdaeb8e7271f254e1cd
Cian O'Sullivan <inventer100@gmail.com>
2020-09-05 13:31:10 +0100

    Add proof of work algorithm and flask server structure

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,4 +1,4 @@
-import time
+from time import time
 import hashlib
 import json
 
@@ -7,20 +7,21 @@ class Blockchain:
     def __init__(self):
         self.chain = []
         self.transactions = []
+        self.new_block(100, 1)
 
-    def new_block(self, proof, previous_hash):
+    def new_block(self, proof, previous_hash=None):
         """ Create a new block """
         current_block = {
-            'index': len(self.chain) + 1,
-            'timestamp': time(),
-            'transactions': self.transactions,
+            'index': len(self.chain) + 1,       # The index of the current block in the chain
+            'timestamp': time(),                # Stored in unix time
+            'transactions': self.transactions,  # The entire list of previous transactions
             'proof': proof,
-            'previous_hash': previous_hash
+            'previous_hash': previous_hash or self.hash_block(self.get_last())
         }
 
         self.transactions.clear()
         self.chain.append(current_block)
-        return self.chain[-1]
+        return current_block
 
     def new_transaction(self, sender, receiver, amount):
         """ Make a new transaction """
@@ -30,7 +31,9 @@ class Blockchain:
             "amount": amount
         })
 
-    def hash(block):
+        return self.get_last()['index'] + 1
+
+    def hash_block(block):
         """ Make a new hash for the given block """
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
@@ -46,3 +49,17 @@ class Blockchain:
     def print_transactions(self):
         for transaction in self.transactions:
             print(transaction)
+
+    def pow(self, last_proof):
+        proof = 0
+        while (self.do_work() is False):
+            proof += 1
+        return proof
+
+    def do_work(last_proof, proof):
+        current_guess = last_proof * proof
+        current_run = str(current_guess).encode()
+        run_hash = hashlib.sha256(current_run).hexdigest()
+        if (run_hash[:4] == "0000"):
+            return True
+        return False
--- src/main.py
+++ src/main.py
@@ -1,6 +1,41 @@
 from blockchain import Blockchain
+from flask import Flask, jsonify
 
-if __name__ == "__main__":
-    chain = Blockchain()
+app = Flask(__name__)
+chain = Blockchain()
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new Block"
+
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return "We'll add a new transaction"
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': chain.chain,        # The actual blockchain
+        'length': len(chain.chain),  # The number of blocks in the chain
+    }
+    return jsonify(response), 200
+
+
+def main():
+    chain.print_chain()
     chain.new_transaction("Cian", "Joan", 50.0)
     chain.print_transactions()
+    chain.print_chain()
+    app.run(host='localhost', port=5000)
+
+
+if __name__ == "__main__":
+    main()
